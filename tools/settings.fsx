9b8c9a194a4dd140e0c02b20dac5027b520017bb
AndreiDegtiarev <Andrey.Degtyarev@gmail.com>
2017-09-17 23:28:49 +0200

    Convert to .Net 4.6

---
+++ tools/settings.fsx
@@ -0,0 +1,5 @@
+module Settings 
+    let projectName = "PropertyGrid"
+    let projectDescription = "WebSharper.Community.PropertyGrid WebSharper native property grid component utilizing reactive variables"
+    let dependencies = [("WebSharper","")]
+    let releaseNotes = ""
