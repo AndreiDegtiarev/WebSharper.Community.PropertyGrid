cc1d1d905ae7cead0914a0965a606a5108a0e35c
AndreiDegtiarev <Andrey.Degtyarev@gmail.com>
2018-04-05 21:45:54 +0200

    package description adjusted

--- tools/settings.fsx
+++ tools/settings.fsx
@@ -1,5 +1,6 @@
 module Settings 
     let projectName = "PropertyGrid"
     let projectDescription = "WebSharper.Community.PropertyGrid WebSharper native property grid component utilizing reactive variables"
-    let dependencies = [("WebSharper","")]
+    let dependencies = [("WebSharper.UI","")
+                        ("WebSharper.FSharp","")]
     let releaseNotes = ""
